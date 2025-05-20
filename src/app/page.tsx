7192582b7055f570e6f69dcaa0630466869344d6
ujinsim <syj11243@naver.com>
2025-05-20 18:28:48 +0900

    delete : 예시 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Badge } from "@/components/Badge";
 export default function Home() {
-  return <div className="flex justify-center text-2xl">Butter-Salt-Flour</div>;
+  return (
+    <div className="flex justify-center text-2xl">
+      Butter-Salt-Flour
+      <Badge />
+    </div>
+  );
 }
