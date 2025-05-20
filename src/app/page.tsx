2dad72c2fa24f263044b1a1a073a520794556ad1
ujinsim <syj11243@naver.com>
2025-05-20 22:34:29 +0900

    feat : shadcn 달력 컴포넌트 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import { Badge } from "@/components/Badge";
-export default function Home() {
+"use client";
+
+import { useState } from "react";
+
+import { Calendar } from "@/components/ui/calendar";
+import { DatePickerDemo } from "@/components/ui/datePicker";
+
+export default function Page() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
   return (
-    <div className="flex justify-center text-2xl">
-      Butter-Salt-Flour
-      <Badge />
-    </div>
+    // <Calendar
+    //   mode="single"
+    //   selected={date}
+    //   onSelect={setDate}
+    //   className="rounded-md border shadow"
+    // />
+    <DatePickerDemo />
   );
 }
