057a811bf7ce93fe2c30d0a44d1d7f2663cba93b
ujinsim <syj11243@naver.com>
2025-05-22 00:52:15 +0900

    delete : 달력 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
 "use client";
 
-import { useState } from "react";
-
-import { Calendar } from "@/components/ui/calendar";
-import { DatePickerDemo } from "@/components/ui/datePicker";
-
 export default function Page() {
-  const [date, setDate] = useState<Date | undefined>(new Date());
-
-  return (
-    // <Calendar
-    //   mode="single"
-    //   selected={date}
-    //   onSelect={setDate}
-    //   className="rounded-md border shadow"
-    // />
-    <DatePickerDemo />
-  );
+  return <></>;
 }
