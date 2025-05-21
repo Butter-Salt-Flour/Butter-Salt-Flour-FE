1e66542a6850e81bbb40f1db12e538b237e581cc
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-05-21 22:31:55 +0900

    <Feat> firebaseAuth setting

---
+++ src/auth/firebase.ts
@@ -0,0 +1,15 @@
+// firebase.ts
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCV1SKqxI2l5OzB4j7jCvvojEvxDQ7Wnyo',
+  authDomain: 'buttersaltflour-auth.firebaseapp.com',
+  projectId: 'buttersaltflour-auth',
+  storageBucket: 'buttersaltflour-auth.appspot.com',
+  messagingSenderId: '212467582253',
+  appId: '1:212467582253:web:25f74fb23c59eed0aa3fa4',
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
