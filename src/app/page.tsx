e3c4035210ab84ca77ab813018b82cf5bc41bde6
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-05-23 03:38:23 +0900

    <Feat> global.css X tailwind X Gloabl coloring 성공

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <div className="bg-prima flex justify-center text-2xl">
       Butter-Salt-Flour
+      <div className="text-banana">banana</div>
+      <div className="bg bg-wonbinBrown"></div>
     </div>
   );
 }
