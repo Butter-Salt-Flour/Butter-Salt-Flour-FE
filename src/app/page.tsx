42e4588e84823c95d869c2749a8b5c44502b7c53
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-05-23 03:41:40 +0900

    <Feat> global coloring at global.css tailwindv4 . DO NOT TOUCH CONFIG JUST USE AT GLOBAL.CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
     <div className="bg-prima flex justify-center text-2xl">
       Butter-Salt-Flour
       <div className="text-banana">banana</div>
-      <div className="bg bg-wonbinBrown"></div>
+      <div className="bg bg-wonbinBrown">wonbinBrown</div>
+      <div className="text-banana">banana</div>
     </div>
   );
 }
